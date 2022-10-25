eaa142539174809bc4cf9234f16820fdde0e2aaa
Felix7895 <felixslayer95@gmail.com>
2022-10-25 08:13:22 +0700

    Exercise4

---
+++ Exercise4_Jedsarit_D.py
@@ -0,0 +1,12 @@
+F = "Foundation English  Score : 10 "
+G = "General Business  Score : 12 "
+I = "Introduction To Computer System  Score : 14 "
+C = "Computer Programmimg  Score : 14 "
+print(F)
+print(G)
+print(I)
+print(C)
+
+
+
+
